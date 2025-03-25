7efc22f0d871b6ff8964a51df5ed6e28db87ade3
monomxt <alejandro.maxit@gmail.com>
2025-03-25 15:19:02 -0300

    cambios

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-# Germán Andrés Xander 2024
-
-d = dht.DHT11(machine.Pin(13))
+from machine import Pin
+import dht
+ 
+d = dht.DHT11(Pin(15))
 d.measure()
 temperatura=d.temperature()
 print(f"\nla temperatura actual es de {temperatura} C")
